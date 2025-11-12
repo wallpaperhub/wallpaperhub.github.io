d6e8f281909cad07055eb8a0d5f03ee65b241499
huythanh1411 <thanhkoi1411@gmail.com>
2025-11-12 17:07:06 +0700

    abc

--- header.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large">
